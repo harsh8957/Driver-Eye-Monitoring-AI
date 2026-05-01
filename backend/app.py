e345a383bf70a667d058e1ed949723e052f4ccea
Harsh Singh <harshsing8957@gmail.com>
2026-05-02 02:27:12 +0530

    all fixes

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # Suppress TensorFlow warnings
 
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
@@ -7,6 +8,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.nn import softmax
 import io
+import uvicorn
 
 app = FastAPI()
 
@@ -18,6 +20,7 @@ def load_model():
     global model
     model = tf.keras.models.load_model("backend/model.h5", compile=False)
     print("Model loaded successfully")
+
 class_names = ['Open Eye', 'Sleepy Eye']
 IMG_SIZE = (150, 150)
 
@@ -28,7 +31,7 @@ async def predict_image(file: UploadFile = File(...)):
     image = image.resize(IMG_SIZE)
     img_array = np.array(image) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
-    
+
     predictions = model.predict(img_array)
     probs = softmax(predictions).numpy()[0]
 
@@ -39,5 +42,6 @@ async def predict_image(file: UploadFile = File(...)):
 
     return JSONResponse(content={"predictions": results})
 
-
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("backend.app:app", host="0.0.0.0", port=port)
